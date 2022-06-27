11f2fb835213ed2bd815bfd5c7ea88cebb902a6d
Mitsuru SHIMAMURA <simamura@iij.ad.jp>
2022-06-27 22:20:34 +0900

    buildx対応, 引数でbind versionを指定するように, dockerhubにpushできるように

--- build.sh
+++ build.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
 
-IS_LATEST=true
-DEBIAN_REL=bullseye
-BIND_VER=9.19.2
+DEBIAN_REL="bullseye"
 
-docker build -t smbd/dig:${BIND_VER} --build-arg DEBIAN_REL=${DEBIAN_REL} --build-arg BIND_VER=${BIND_VER} .
+while getopts pt OPT ; do
+  case ${OPT} in
+    "l") LATEST="true" ;;
+    "p") PUSH="true" ;;
+  esac
+done
 
-if "${IS_LATEST}" == true ; then
-  docker build -t smbd/dig:latest --build-arg DEBIAN_REL=${DEBIAN_REL} --build-arg BIND_VER=${BIND_VER} .
+shift $((${OPTIND}-1))
+
+if [ "$1" == "" ] ; then
+  echo "Usage: ${0} [-l] [-p] BIND_VERSION"
+  echo "    -l: update latest tag"
+  echo "    -p: push to dockerhub"
+  exit 1
+fi
+
+BIND_VER="$1"
+
+if [ "${PUSH}" == "true" ] ; then
+  docker buildx build --push --platform linux/amd64,linux/arm64 -t smbd/dig:${BIND_VER} --build-arg DEBIAN_REL=${DEBIAN_REL} --build-arg BIND_VER=${BIND_VER} .
+
+  if [ "${LATEST}" == "true" ] ; then
+    docker buildx build --push --platform linux/amd64,linux/arm64 -t smbd/dig:latest --build-arg DEBIAN_REL=${DEBIAN_REL} --build-arg BIND_VER=${BIND_VER} .
+  fi
+else
+  docker buildx build --load -t smbd/dig:${BIND_VER} --build-arg DEBIAN_REL=${DEBIAN_REL} --build-arg BIND_VER=${BIND_VER} .
+
+  if [ "${LATEST}" == "true" ] ; then
+    docker buildx build --load -t smbd/dig:latest --build-arg DEBIAN_REL=${DEBIAN_REL} --build-arg BIND_VER=${BIND_VER} .
+  fi
 fi
